2758a8f84f38204ee6e5d557d32c780a64b0d8b2
Lazytangent <pm123633@gmail.com>
2021-03-03 13:12:04 -0600

    Configure more tailwind settings

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -1,7 +1,25 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      green: {
+        DEFAULT: '#00ce86',
+        dark: '#007a50',
+      },
+      purple: {
+        DEFAULT: '#9556dc',
+        dark: '#391463',
+      },
+    },
+    fontFamily: {
+      big: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      sans: ['"Carrois Gothic"', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   variants: {
