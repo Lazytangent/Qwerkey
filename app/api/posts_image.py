85a7aabadf28d60f3a7542d123d801c6fed9d88e
Lazytangent <pm123633@gmail.com>
2021-03-03 23:03:20 -0600

    Draft delete one posts image route

---
+++ app/api/posts_image.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from app.models import db, Post, PostsImage
+
+posts_image_routes = Blueprint('posts_image', __name__)
+
+
+@posts_image_routes.route('/<int:posts_image_id>', methods=["DELETE"])
+def delete_posts_image(posts_image_id):
+    image = PostsImage.query.get(posts_image_id)
+    post = Post.query.get(image.post_id)
+    db.session.delete(image)
+    db.session.commit()
+    return post.to_dict()
