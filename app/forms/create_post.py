95d6035f2e95d913763a8d88f4a76d4c9678c82b
Lazytangent <pm123633@gmail.com>
2021-03-04 10:20:09 -0600

    Update CreatePost FlaskForm

--- app/forms/create_post.py
+++ app/forms/create_post.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
 
 
 class CreatePost(FlaskForm):
     title = StringField(validators=[DataRequired()])
     body = TextAreaField()
+    user_id = IntegerField(validators=[DataRequired()])
+    community_id = IntegerField(validators=[DataRequired()])
