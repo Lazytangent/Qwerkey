da5da05a9c411d2d886f2921fa6a17cfa9f09ed8
Lazytangent <pm123633@gmail.com>
2021-03-03 20:58:52 -0600

    Start drafting routes

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -2,3 +2,12 @@ from flask import Blueprint, jsonify, request
 
 post_routes = Blueprint('posts', __name__)
 
+
+@post_routes.route('/', methods=["GET", "POST"])
+def get_posts():
+    pass
+
+
+@post_routes.route('/<int:post_id>', methods=["PUT", "DELETE"])
+def update_post(post_id):
+    pass
