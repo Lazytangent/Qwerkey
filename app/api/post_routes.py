206ab181eeff6f70b7380a765e350ff9606936cb
Lazytangent <pm123633@gmail.com>
2021-03-03 21:03:36 -0600

    Write GET route for posts

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, jsonify, request
+from app.models import db, Post
 
 post_routes = Blueprint('posts', __name__)
 
 
 @post_routes.route('/', methods=["GET", "POST"])
 def get_posts():
-    pass
+    if request.method == "GET":
+        posts = Post.query.all()
+        return {post.id: post.to_dict() for post in posts}
+    form = CreatePost()
+    if form.validate_on_submit():
+        pass
 
 
 @post_routes.route('/<int:post_id>', methods=["PUT", "DELETE"])
