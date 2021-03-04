cf6e09aa91d52c61ba829d1d6a814cd4f7710b44
Lazytangent <pm123633@gmail.com>
2021-03-03 21:27:41 -0600

    Pagination on Get route

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -8,8 +8,9 @@ post_routes = Blueprint('posts', __name__)
 @post_routes.route('/', methods=["GET", "POST"])
 def get_posts():
     if request.method == "GET":
-        posts = Post.query.all()
-        return {post.id: post.to_dict() for post in posts}
+        page = int(request.args.get('page'))
+        posts = Post.query.paginate(page=page, per_page=20)
+        return {post.id: post.to_dict() for post in posts.items}
     form = CreatePost()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
