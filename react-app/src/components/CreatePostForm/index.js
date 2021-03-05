8284c4166b799b2c936523b508e28227ba0836a9
Lazytangent <pm123633@gmail.com>
2021-03-04 18:20:03 -0600

    Draft CreatePostModal

--- react-app/src/components/CreatePostForm/index.js
+++ react-app/src/components/CreatePostForm/index.js
@@ -1,3 +1,20 @@
+import { useState } from 'react';
+
+import { Modal } from '../../context/ModalContext';
 import CreatePostForm from './CreatePostForm';
 
-export default CreatePostForm;
+const CreatePostModal = () => {
+  const [showCreatePostModal, setShowCreatePostModal] = useState(false);
+
+  return (
+    <>
+      {showCreatePostModal && (
+        <Modal onClose={() => setShowCreatePostModal(false)}>
+          <CreatePostForm />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default CreatePostModal;
