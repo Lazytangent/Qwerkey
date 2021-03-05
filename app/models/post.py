70d398446fb5fa28568b6a71c22693e57337b66a
Lazytangent <pm123633@gmail.com>
2021-03-05 14:56:38 -0600

    Formatting

--- app/models/post.py
+++ app/models/post.py
@@ -9,7 +9,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     community_id = db.Column(db.Integer,
-                             db.ForeignKey("communities.id"), nullable=False)
+                             db.ForeignKey("communities.id"),
+                             nullable=False)
     title = db.Column(db.String(100), nullable=False)
     body = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
@@ -38,6 +39,8 @@ class Post(db.Model):
             "body": self.body,
             "images": [image.image_url for image in self.images],
             "tags": [tag.name for tag in self.tags],
-            "threads": {thread.id: thread.to_dict() for thread in self.threads},
+            "threads":
+            {thread.id: thread.to_dict()
+             for thread in self.threads},
             "created_at": self.created_at
         }
