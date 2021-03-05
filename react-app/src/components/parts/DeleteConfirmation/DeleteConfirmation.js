8eb75c30be9da09789c9d8362d3d28bd985e30a7
Lazytangent <pm123633@gmail.com>
2021-03-04 21:26:22 -0600

    Start Delete button functionality

---
+++ react-app/src/components/parts/DeleteConfirmation/DeleteConfirmation.js
@@ -0,0 +1,9 @@
+const DeleteConfirmation = ({ setShowDeleteModal }) => {
+  return (
+    <div>
+      <h3>Placeholder for Delete Confirmation</h3>
+    </div>
+  );
+};
+
+export default DeleteConfirmation;
