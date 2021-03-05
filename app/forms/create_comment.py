6bd1114284a7d40343a938b35c7ce8991ffbf04a
Lazytangent <pm123633@gmail.com>
2021-03-05 15:17:42 -0600

    Update CreateComment Form

--- app/forms/create_comment.py
+++ app/forms/create_comment.py
@@ -6,7 +6,8 @@ from wtforms.validators import DataRequired
 class CreateComment(FlaskForm):
     body = TextAreaField(validators=[DataRequired()])
     user_id = IntegerField(validators=[DataRequired()])
-    thread_id = IntegerField(validators=[DataRequired()])
-    comment_id = IntegerField(validators=[DataRequired()])
-    path = StringField(validators=[DataRequired()])
-    level = StringField(validators=[DataRequired()])
+    post_id = IntegerField(validators=[DataRequired()])
+    thread_id = IntegerField()
+    comment_id = IntegerField()
+    path = StringField()
+    level = StringField()
