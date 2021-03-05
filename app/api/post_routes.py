d69e3e35240dbe460ff8ba4a5e5e3e97ca956723
Lazytangent <pm123633@gmail.com>
2021-03-05 12:46:58 -0600

    Cleanup

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -15,13 +15,10 @@ def get_posts():
     page = int(request.args.get('page', 0))
     community_name = request.args.get('community_name', '')
     if community_name:
-        print(community_name)
         community = Community.query.filter(
             Community.name.ilike(community_name)).one()
-        print(community)
         posts = Post.query.filter(Post.community_id == community.id).paginate(
             page=page, per_page=20)
-        print(posts)
     else:
         posts = Post.query.paginate(page=page, per_page=20)
     return {post.id: post.to_dict() for post in posts.items}
