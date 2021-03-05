2a2a4ccc06143ea0bb9beefd5412d5e94c67d57e
Lazytangent <pm123633@gmail.com>
2021-03-05 12:22:20 -0600

    Styling on Delete Confirmation

--- react-app/src/components/parts/DeleteButton/DeleteButton.js
+++ react-app/src/components/parts/DeleteButton/DeleteButton.js
@@ -1,7 +1,7 @@
-const DeleteButton = ({ label, onClick, children }) => {
+const DeleteButton = ({ className, label, onClick, children }) => {
   return (
     <>
-      <button onClick={onClick} className="p-2 mx-2 border rounded hover:border-red-500 ease-in-out focus:bg-red-500 focus:outline-none">{label}</button>
+      <button onClick={onClick} className={`${className} p-2 mx-2 border rounded hover:border-red-500 ease-in-out focus:bg-red-500 focus:outline-none`}>{label}</button>
       { children }
     </>
   );
--- react-app/src/components/parts/DeleteConfirmation/DeleteConfirmation.js
+++ react-app/src/components/parts/DeleteConfirmation/DeleteConfirmation.js
@@ -1,6 +1,8 @@
 import { useDispatch } from 'react-redux';
 
 import { deletePost } from '../../../store/posts';
+import DeleteButton from '../DeleteButton';
+import EditButton from '../EditButton';
 
 const DeleteConfirmation = ({ setShowDeleteModal, postId }) => {
   const dispatch = useDispatch();
@@ -15,10 +17,14 @@ const DeleteConfirmation = ({ setShowDeleteModal, postId }) => {
   };
 
   return (
-    <div>
-      <h4>Are you sure?</h4>
-      <button onClick={deletePostHandler}>Yes</button>
-      <button onClick={cancelDeleteHandler}>No</button>
+    <div className="p-4 pb-6 bg-white rounded md:w-60">
+      <div className="flex justify-center p-2">
+        <h4>Are you sure?</h4>
+      </div>
+      <div className="flex justify-around">
+        <DeleteButton className="w-1/3 hover:bg-red-500" onClick={deletePostHandler} label="Yes" />
+        <EditButton className="w-1/3 hover:bg-yellow-500" onClick={cancelDeleteHandler} label="No" />
+      </div>
     </div>
   );
 };
