77476f0b287c6d1580d49941bab1a3b198d9768f
Lazytangent <pm123633@gmail.com>
2021-03-05 17:10:57 -0600

    Update styling on modal

--- react-app/src/context/ModalContext.js
+++ react-app/src/context/ModalContext.js
@@ -28,7 +28,7 @@ export const Modal = ({ onClose, children }) => {
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center">
       <div className="fixed inset-0 bg-black bg-opacity-70" onClick={onClose} />
-      <div className="absolute w-auto bg-black top-1/3">
+      <div className="absolute w-auto my-auto bg-black">
         {children}
       </div>
     </div>,
