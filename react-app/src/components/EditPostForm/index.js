5ed86153e98aa9e155e1d3820ac071a9122e6c46
Lazytangent <pm123633@gmail.com>
2021-03-05 11:12:07 -0600

    Write EditPostModal

--- react-app/src/components/EditPostForm/index.js
+++ react-app/src/components/EditPostForm/index.js
@@ -1,3 +1,16 @@
+import { Modal } from '../../context/ModalContext';
 import EditPostForm from './EditPostForm';
 
-export default EditPostForm;
+const EditPostModal = ({ setShowEditModal, showEditModal }) => {
+  return (
+    <>
+      {showEditModal && (
+        <Modal onClose={() => setShowEditModal(false)}>
+          <EditPostForm setShowEditModal={setShowEditModal} />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default EditPostModal;
