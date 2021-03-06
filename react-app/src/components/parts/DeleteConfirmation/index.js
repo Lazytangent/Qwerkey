d53378a594c3ac27b6d05bd5ccd566b637cbaff1
Lazytangent <pm123633@gmail.com>
2021-03-06 14:55:19 -0600

    Working delete comment functionality

--- react-app/src/components/parts/DeleteConfirmation/index.js
+++ react-app/src/components/parts/DeleteConfirmation/index.js
@@ -1,12 +1,12 @@
 import { Modal } from '../../../context/ModalContext';
 import DeleteConfirmation from './DeleteConfirmation';
 
-const DeleteConfirmationModal = ({ showDeleteModal, setShowDeleteModal, postId }) => {
+const DeleteConfirmationModal = ({ showDeleteModal, setShowDeleteModal, postId, commentId }) => {
   return (
     <>
       {showDeleteModal && (
         <Modal onClose={() => setShowDeleteModal(false)}>
-          <DeleteConfirmation setShowDeleteModal={setShowDeleteModal} postId={postId} />
+          <DeleteConfirmation setShowDeleteModal={setShowDeleteModal} postId={postId} commentId={commentId} />
         </Modal>
       )}
     </>
