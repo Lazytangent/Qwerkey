43e9035603b1d0d38e8daefe9d9b9ed5a096ae5f
Lazytangent <pm123633@gmail.com>
2021-03-05 18:44:08 -0600

    Edit CreateCommentForm

--- react-app/src/components/CreateCommentForm/CreateCommentForm.js
+++ react-app/src/components/CreateCommentForm/CreateCommentForm.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
+import InputField from "../parts/InputField";
+import SubmitFormButton from "../parts/SubmitFormButton";
+
 const CreateCommentForm = () => {
+  const [body, setBody] = useState("");
+
+  const updateBody = (e) => {
+    setBody(e.target.value);
+  };
+
+  const submitComment = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <>
-      <h3>Placeholder for CreateCommentForm</h3>
-    </>
+    <div className="p-2 mb-2 rounded">
+      <form onSubmit={submitComment}>
+        <InputField
+          name="commentBody"
+          type="textarea"
+          placeholder="Make a Comment"
+          value={body}
+          onChange={updateBody}
+        />
+        <SubmitFormButton label="Submit Comment" />
+      </form>
+    </div>
   );
 };
 
--- react-app/src/components/Post/Post.js
+++ react-app/src/components/Post/Post.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-import EditButton from '../parts/EditButton';
-import DeleteButton from '../parts/DeleteButton';
-import EditPostModal from '../EditPostForm';
-import DeleteConfirmationModal from '../parts/DeleteConfirmation';
-import CreateCommentForm from '../CreateCommentForm';
+import EditButton from "../parts/EditButton";
+import DeleteButton from "../parts/DeleteButton";
+import EditPostModal from "../EditPostForm";
+import DeleteConfirmationModal from "../parts/DeleteConfirmation";
+import CreateCommentForm from "../CreateCommentForm";
 
 const Post = ({ post, userId }) => {
   const [showEditModal, setShowEditModal] = useState(false);
@@ -20,29 +20,43 @@ const Post = ({ post, userId }) => {
   };
 
   return (
-    <div key={post.id} className="p-2 mb-2 rounded shadow-sm hover:shadow-lg">
-      <NavLink to={`/q/${post.community.name}/${post.id}`}><h3 className="hover:underline">{post.title}</h3></NavLink>
-      <p>{post.body}</p>
-      <p>{post.created_at}</p>
-      {post.tags.map(tag => (
-        <p>{tag}</p>
-      ))}
-      {post.images.map(url => (
-        <img src={url} alt={`for ${post.title}`} key={url}/>
-      ))}
-      {userId && post.user.id !== userId && (
-        <CreateCommentForm postId={post.id} />
-      )}
-      {post.user.id === userId && (
-        <>
-          <EditButton label="Edit Post" onClick={editBtnHandler}>
-            <EditPostModal showEditModal={showEditModal} setShowEditModal={setShowEditModal} postId={post.id} />
-          </EditButton>
-          <DeleteButton label="Delete Post" onClick={deleteBtnHandler}>
-            <DeleteConfirmationModal showDeleteModal={showDeleteModal} setShowDeleteModal={setShowDeleteModal} postId={post.id} />
-          </DeleteButton>
-        </>
-      )}
+    <div>
+      <div key={post.id} className="p-2 mb-2 rounded shadow-sm hover:shadow-lg">
+        <NavLink to={`/q/${post.community.name}/${post.id}`}>
+          <h3 className="hover:underline">{post.title}</h3>
+        </NavLink>
+        <p>{post.body}</p>
+        <p>{post.created_at}</p>
+        {post.tags.map((tag) => (
+          <p>{tag}</p>
+        ))}
+        {post.images.map((url) => (
+          <img src={url} alt={`for ${post.title}`} key={url} />
+        ))}
+        {post.user.id === userId && (
+          <>
+            <EditButton label="Edit Post" onClick={editBtnHandler}>
+              <EditPostModal
+                showEditModal={showEditModal}
+                setShowEditModal={setShowEditModal}
+                postId={post.id}
+              />
+            </EditButton>
+            <DeleteButton label="Delete Post" onClick={deleteBtnHandler}>
+              <DeleteConfirmationModal
+                showDeleteModal={showDeleteModal}
+                setShowDeleteModal={setShowDeleteModal}
+                postId={post.id}
+              />
+            </DeleteButton>
+          </>
+        )}
+      </div>
+      <div>
+        {userId && post.user.id !== userId && (
+          <CreateCommentForm postId={post.id} />
+        )}
+      </div>
     </div>
   );
 };
