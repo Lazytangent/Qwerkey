5c26567cf4e19f11c035910c9190335cfc0bd591
Lazytangent <pm123633@gmail.com>
2021-03-06 23:20:57 -0600

    Updated styling for toggle

--- react-app/src/components/DarkModeToggle/DarkModeToggle.js
+++ react-app/src/components/DarkModeToggle/DarkModeToggle.js
@@ -8,12 +8,15 @@ const DarkModeToggle = () => {
   };
 
   return (
-    <>
-      <button onClick={toggleDarkMode}>
-        {isDarkMode ? "Dark Mode" : "Light Mode"}
-      </button>
-    </>
-  )
+    <div className="flex justify-between items-center px-8 py-6">
+      <p>{isDarkMode ? "Dark Mode" : "Light Mode"}</p>
+      {/* <input type="checkbox" value={isDarkMode} className="w-16 h-10 bg-gray-300 rounded-full flex-shrink-0 p-1" onChange={toggleDarkMode} /> */}
+      <div className="w-16 h-10 bg-gray-300 rounded-full flex-shrink-0 p-1">
+        <div className="bg-white w-8 h-8 rounded-full shadow-md"></div>
+      </div>
+      <span></span>
+    </div>
+  );
 };
 
 export default DarkModeToggle;
