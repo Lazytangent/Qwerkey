c68654bc51acf303dd283776f29d8c560404d8ad
Lazytangent <pm123633@gmail.com>
2021-03-07 15:33:06 -0600

    Import forms

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,5 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .create_post import CreatePost
 from .create_comment import CreateComment
+from .create_retailer import CreateRetailer
+from .create_retailer_rating import CreateRetailerRating
