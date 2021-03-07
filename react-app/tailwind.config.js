cc0fbc5840e48e2e3e2bdb97d3f09f4a14bd5e75
Lazytangent <pm123633@gmail.com>
2021-03-06 18:30:40 -0600

    Update tailwind config for dark mode

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     colors: {
       ...colors,
