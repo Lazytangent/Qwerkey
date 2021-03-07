716836f19c59b0148e03af093db7e61ceae49fc6
Lazytangent <pm123633@gmail.com>
2021-03-06 18:50:48 -0600

    Restructure NavBar to include DarkModeToggle

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,27 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import { useAuthContext } from '../../context/AuthContext';
-import { useCreatePostContext } from '../../context/CreatePostContext';
-import LoginModal from '../LoginForm';
-import SignUpModal from '../SignUpForm';
-import LogoutButton from '../LogoutButton';
-import CreateFormModal from '../CreatePostForm';
-import NavButton from '../parts/NavButton';
+import { useAuthContext } from "../../context/AuthContext";
+import { useCreatePostContext } from "../../context/CreatePostContext";
+import LoginModal from "../LoginForm";
+import SignUpModal from "../SignUpForm";
+import LogoutButton from "../LogoutButton";
+import CreateFormModal from "../CreatePostForm";
+import DarkModeToggle from "../DarkModeToggle";
+import NavButton from "../parts/NavButton";
 
 const NavBar = () => {
-  const { setShowLoginModal, setShowSignUpModal, authenticated } = useAuthContext();
+  const {
+    setShowLoginModal,
+    setShowSignUpModal,
+    authenticated,
+  } = useAuthContext();
   const { setShowCreatePostModal } = useCreatePostContext();
 
   const createPostBtnHandler = () => {
-    setShowCreatePostModal(prev => !prev);
-  }
+    setShowCreatePostModal((prev) => !prev);
+  };
 
   const loginBtnClickHandler = () => {
-    setShowLoginModal(prev => !prev);
+    setShowLoginModal((prev) => !prev);
   };
 
   const signUpBtnClickHandler = () => {
-    setShowSignUpModal(prev => !prev);
+    setShowSignUpModal((prev) => !prev);
   };
 
   return (
@@ -29,37 +34,45 @@ const NavBar = () => {
       <ul className="grid grid-cols-3">
         <div className="flex p-2">
           <li>
-            <NavLink className="p-2 align-middle hover:underline hover:text-purple-dark" to="/" exact={true} activeClassName="active">
+            <NavLink
+              className="p-2 align-middle hover:underline hover:text-purple-dark"
+              to="/"
+              exact={true}
+              activeClassName="active"
+            >
               Home
             </NavLink>
           </li>
         </div>
-        {!authenticated && (
-          <div className="flex justify-end col-start-3">
-            <li>
-              <NavButton name="Login" onClick={loginBtnClickHandler}>
-                <LoginModal />
-              </NavButton>
-            </li>
-            <li>
-              <NavButton name="Sign Up" onClick={signUpBtnClickHandler}>
-                <SignUpModal />
-              </NavButton>
-            </li>
-          </div>
-        )}
-        {authenticated && (
-          <div className="flex justify-end col-start-3">
-            <li>
-              <NavButton name="Create Post" onClick={createPostBtnHandler}>
-                <CreateFormModal />
-              </NavButton>
-            </li>
-            <li>
-              <LogoutButton />
-            </li>
-          </div>
-        )}
+        <div className="flex justify-end col-start-3">
+          <DarkModeToggle />
+          {!authenticated && (
+            <>
+              <li>
+                <NavButton name="Login" onClick={loginBtnClickHandler}>
+                  <LoginModal />
+                </NavButton>
+              </li>
+              <li>
+                <NavButton name="Sign Up" onClick={signUpBtnClickHandler}>
+                  <SignUpModal />
+                </NavButton>
+              </li>
+            </>
+          )}
+          {authenticated && (
+            <>
+              <li>
+                <NavButton name="Create Post" onClick={createPostBtnHandler}>
+                  <CreateFormModal />
+                </NavButton>
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+            </>
+          )}
+        </div>
       </ul>
     </nav>
   );
