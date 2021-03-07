0374ea98ee8ce9eb9c987d9732f05e000ab8e29f
Lazytangent <pm123633@gmail.com>
2021-03-07 15:38:15 -0600

    Create update_rating routing

--- app/api/retailer_routes.py
+++ app/api/retailer_routes.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from flask import Blueprint, request
 
+from app.helpers import validation_errors_to_error_messages
 from app.forms import CreateRetailerRating
 from app.models import db, Retailer, RetailerRating
 
@@ -24,3 +26,25 @@ def post_rating(retailer_id):
         db.session.add(rating)
         db.session.commit()
         return retailer.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}
+
+
+@retailer_routes.route('/<int:retailer_id>/ratings/<int:rating_id>',
+                       methods=["PUT", "DELETE"])
+def update_rating(retailer_id, rating_id):
+    retailer = Retailer.query.get(retailer_id)
+    rating = RetailerRating.query.get(rating_id)
+    if request.method == "PUT":
+        form = CreateRetailerRating()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            rating.rating = form['rating'].data
+            rating.updated_at = datetime.utcnow()
+            db.session.commit()
+            return retailer.to_dict()
+        return {"errors": validation_errors_to_error_messages(form.errors)}
+    elif request.method == "DELETE":
+        db.session.delete(rating)
+        db.session.commit()
+        return retailer.to_dict()
+    return {"message": "Bad route"}
