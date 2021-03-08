8fb642321eaf4d17bc9c5712e6d84e277430c16b
Lazytangent <pm123633@gmail.com>
2021-03-07 19:05:31 -0600

    Create RetailerPage component

---
+++ react-app/src/components/RetailerPage/RetailerPage.js
@@ -0,0 +1,9 @@
+const RetailerPage = () => {
+  return (
+    <>
+      <h2>Placeholder for RetailerPage</h2>
+    </>
+  );
+};
+
+export default RetailerPage;
---
+++ react-app/src/components/RetailerPage/index.js
@@ -0,0 +1,3 @@
+import RetailerPage from './RetailerPage';
+
+export default RetailerPage;
