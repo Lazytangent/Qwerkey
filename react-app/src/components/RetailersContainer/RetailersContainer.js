090b3a817c51a8591237d21b0a82f880ad729e2e
Lazytangent <pm123633@gmail.com>
2021-03-07 19:01:07 -0600

    Draft RetailersContainer component

--- react-app/src/components/RetailersContainer/RetailersContainer.js
+++ react-app/src/components/RetailersContainer/RetailersContainer.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import Retailer from "../Retailer";
+
 const RetailersContainer = () => {
+  const retailers = useSelector(state => state.retailers);
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (retailers) {
+      setIsLoaded(true);
+    }
+  }, [retailers]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <>
       <h2>Placeholder for RetailersContainer</h2>
+      {Object.values(retailers).map(retailer => (
+        <>
+          <Retailer retailer={retailer} />
+        </>
+      ))}
     </>
   );
 };
