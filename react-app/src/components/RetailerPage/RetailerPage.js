e05c64fddb5dc802f306a374d27c27759cec12d0
Lazytangent <pm123633@gmail.com>
2021-03-07 20:26:40 -0600

    Layout RetailerPage

--- react-app/src/components/RetailerPage/RetailerPage.js
+++ react-app/src/components/RetailerPage/RetailerPage.js
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import Retailer from "../Retailer";
+
 const RetailerPage = () => {
+  const { retailerId } = useParams();
+  const retailer = useSelector(state => state.retailer ? state.retailers[retailerId] : null);
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (retailer) {
+      setIsLoaded(true);
+    }
+  }, [retailer]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <>
       <h2>Placeholder for RetailerPage</h2>
+      {retailer && (
+        <>
+          <Retailer retailer={retailer} />
+        </>
+      )}
     </>
   );
 };
