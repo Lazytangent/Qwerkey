21da46d9df8eb7c4c8a24bdb3bba4c23ea0d0cb3
Lazytangent <pm123633@gmail.com>
2021-03-08 12:14:32 -0600

    Update formatting of error message

--- app/forms/create_retailer_rating.py
+++ app/forms/create_retailer_rating.py
@@ -6,7 +6,5 @@ from wtforms.validators import DataRequired, NumberRange
 class CreateRetailerRating(FlaskForm):
     user_id = IntegerField(validators=[DataRequired()])
     rating = IntegerField(validators=[
-        NumberRange(min=1,
-                    max=5,
-                    message="Must be between 1 and 5")
+        NumberRange(min=1, max=5, message="Must be between 1 and 5")
     ])
