06344b5b798736b031c1f8abe0cd386601067a46
Lazytangent <pm123633@gmail.com>
2021-03-08 22:41:43 -0600

    Update preliminary search route

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,13 +1,31 @@
 from flask import Blueprint, request
+from sqlalchemy import or_
+
+from app.models import Post, Comment, Retailer
 
 search_routes = Blueprint("search", __name__)
 
 
 @search_routes.route('')
 def search_function():
-    pass
+    query = request.args.get("query")
+    posts = Post.query.filter(
+        or_(Post.title.ilike(f"%{query}%"),
+            Post.body.ilike(f"%{query}%"))).all()
+    comments = Comment.query.filter(Comment.body.ilike(f"%{query}%")).all()
+    retailers = Retailer.query.filter(
+        or_(Retailer.name.ilike(f"%{query}%"),
+            Retailer.description.ilike(f"%{query}%"))).all()
+    return {
+        "posts": [post.to_dict() for post in posts],
+        "comments": [comment.to_dict() for comment in comments],
+        "retailers": [retailer.to_dict() for retailer in retailers]
+    }
 
 
-@search_routes.route('/')
+@search_routes.route('/posts')
 def search_function_filter():
-    pass
+    query = request.args.get("query")
+    field = request.args.get("field")
+    if field:
+        pass
