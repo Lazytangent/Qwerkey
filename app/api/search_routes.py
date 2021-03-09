859bf8d6450d9b87e0b6cef03c938d7d1ec4d1ac
Lazytangent <pm123633@gmail.com>
2021-03-08 21:02:52 -0600

    Start search routing

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,3 +1,13 @@
 from flask import Blueprint, request
 
 search_routes = Blueprint("search", __name__)
+
+
+@search_routes.route('')
+def search_function():
+    pass
+
+
+@search_routes.route('/')
+def search_function_filter():
+    pass
