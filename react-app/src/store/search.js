caf05f9ba8974dd8d2f4bc3cba1bbeca843eb18a
Lazytangent <pm123633@gmail.com>
2021-03-09 12:51:03 -0600

    Dispatch the action creators

--- react-app/src/store/search.js
+++ react-app/src/store/search.js
@@ -25,6 +25,11 @@ const setRetailers = (retailers) => {
 
 export const getQuery = (queryString, type, field) => async (dispatch) => {
   const res = await fetch(`/api/search?query=${queryString}${type ? `&type=${type}` : ""}${field ? `&field=${field}` : ""}`);
+  const data = await res.json();
+  dispatch(setPosts(data.posts));
+  dispatch(setComments(data.comments));
+  dispatch(setRetailers(data.retailers));
+  return data;
 };
 
 const initialState = {
