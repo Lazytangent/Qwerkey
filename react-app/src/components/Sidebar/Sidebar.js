33e1f2885b28591ae7e171b2ffb40d7de619b4d2
Lazytangent <pm123633@gmail.com>
2021-03-10 11:44:29 -0600

    Fix rename

--- react-app/src/components/Sidebar/Sidebar.js
+++ react-app/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getPopularCommunities } from "../../store/sidebar";
+
 const Sidebar = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPopularCommunities());
+  })
+
   return (
     <div>
       <h3>Placeholder for sidebar</h3>
