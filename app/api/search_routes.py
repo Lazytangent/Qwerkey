96db1c36e0d818b5dba19f2f1ca2251cc62477c3
Lazytangent <pm123633@gmail.com>
2021-03-09 22:37:00 -0600

    Edit search api route to include query string parameters

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -9,10 +9,56 @@ search_routes = Blueprint("search", __name__)
 @search_routes.route('')
 def search_function():
     query = request.args.get("query")
+    type_ = request.args.get("type", "all")
+    field = request.args.get("field", "all")
+    if type_ == "Post":
+        if field == "title":
+            posts = \
+                Post.query.filter(Post.title.ilike(
+                    f"%{query}%")).limit(5).all()
+            return {"posts": [post.to_dict() for post in posts]}
+        elif field == "body":
+            posts = \
+                Post.query.filter(Post.body.ilike(
+                    f"%{query}%")).limit(5).all()
+            return {"posts": [post.to_dict() for post in posts]}
+        else:
+            posts = Post.query.filter(
+                or_(Post.title.ilike(f"%{query}%"),
+                    Post.body.ilike(f"%{query}%"))).limit(5).all()
+            return {"posts": [post.to_dict() for post in posts]}
+    elif type_ == "Comment":
+        comments = Comment.query.filter(
+            Comment.body.ilike(f"%{query}%")).limit(5).all()
+        return {"comments": [comment.to_dict() for comment in comments]}
+    elif type_ == "Retailer":
+        if field == "name":
+            retailers = \
+                Retailer.query.filter(Retailer.name.ilike(
+                    f"%{query}%")).limit(5).all()
+            return {
+                "retailers": [retailer.to_dict() for retailer in retailers]
+            }
+        elif field == "description":
+            retailers = \
+                Retailer.query.filter(Retailer.description.ilike(
+                    f"%{query}%")).limit(5).all()
+            return {
+                "retailers": [retailer.to_dict() for retailer in retailers]
+            }
+        else:
+            retailers = Retailer.query.filter(
+                or_(Retailer.name.ilike(f"%{query}%"),
+                    Retailer.description.ilike(f"%{query}%"))).limit(5).all()
+            return {
+                "retailers": [retailer.to_dict() for retailer in retailers]
+            }
+
     posts = Post.query.filter(
         or_(Post.title.ilike(f"%{query}%"),
             Post.body.ilike(f"%{query}%"))).limit(5).all()
-    comments = Comment.query.filter(Comment.body.ilike(f"%{query}%")).limit(5).all()
+    comments = Comment.query.filter(
+        Comment.body.ilike(f"%{query}%")).limit(5).all()
     retailers = Retailer.query.filter(
         or_(Retailer.name.ilike(f"%{query}%"),
             Retailer.description.ilike(f"%{query}%"))).limit(5).all()
@@ -21,28 +67,3 @@ def search_function():
         "comments": [comment.to_dict() for comment in comments],
         "retailers": [retailer.to_dict() for retailer in retailers]
     }
-
-
-@search_routes.route('/posts')
-def search_posts():
-    query = request.args.get("query")
-    field = request.args.get("field")
-    if field == "title":
-        pass
-    elif field == "body":
-        pass
-    else:
-        posts = Post.query.filter(
-            or_(Post.title.ilike(f"%{query}%"),
-                Post.body.ilike(f"%{query}%"))).limit(5).all()
-        return {"posts": [post.to_dict() for post in posts]}
-
-
-@search_routes.route('/comments')
-def search_comments():
-    pass
-
-
-@search_routes.route('/retailers')
-def search_retailers():
-    pass
