9c3ac3cab7ad877758d82b23c10f101a35fba45a
Lazytangent <pm123633@gmail.com>
2021-03-10 10:29:53 -0600

    Create routes for getting community information

--- app/api/community_routes.py
+++ app/api/community_routes.py
@@ -1,3 +1,20 @@
 from flask import Blueprint, request
+from sqlalchemy import desc, func
+
+from app.models import Community, Post
 
 community_routes = Blueprint("communities", __name__)
+
+
+@community_routes.route('/')
+def get_communities():
+    communities = Community.query.join(Post). \
+        group_by(Community.id). \
+        order_by(desc(func.count(Post.community_id))).limit(5).all()
+    return {community.id: community.to_dict() for community in communities}
+
+
+@community_routes.route('/<int:community_id>')
+def get_community(community_id):
+    community = Community.query.get(community_id).first()
+    return community.to_dict()
