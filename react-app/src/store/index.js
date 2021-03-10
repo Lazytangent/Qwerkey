4436a4661dd92b8eb3301b87c091ce2d8f4e59e1
Lazytangent <pm123633@gmail.com>
2021-03-10 11:03:35 -0600

    Import communities reducer into rootReducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -5,12 +5,14 @@ import sessionReducer from './session';
 import postsReducer from './posts';
 import retailersReducer from './retailers';
 import searchReducer from "./search";
+import communitiesReducer from "./communities";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   posts: postsReducer,
   retailers: retailersReducer,
   search: searchReducer,
+  communities: communitiesReducer,
 });
 
 let enhancer;
