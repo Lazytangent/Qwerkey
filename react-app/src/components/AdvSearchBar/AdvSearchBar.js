d2f290d9fa77a366e75e0ab3ce1f5812a0aa8625
Lazytangent <pm123633@gmail.com>
2021-03-09 18:34:43 -0600

    Draft adv search

--- react-app/src/components/AdvSearchBar/AdvSearchBar.js
+++ react-app/src/components/AdvSearchBar/AdvSearchBar.js
@@ -18,13 +18,29 @@ const AdvSearchBar = () => {
     setSearchInput(e.target.value);
   };
 
-  const submitHandler = (e) => {
+  const updateType = (e) => {
+    setType(e.target.value);
+  };
+
+  const updateField = (e) => {
+    setField(e.target.value);
+  };
+
+  const submitHandler = async (e) => {
     e.preventDefault();
+    await dispatch(getQuery(searchInput, type, field));
+    history.push(`/search?query=${searchInput}${type ? `&type=${type}` : ""}${field ? `&field=${field}` : ""}`);
+    setSearched(true);
   };
 
   return (
     <div className="p-2">
       <h3>Placeholder for AdvSearchBar</h3>
+      <form onSubmit={submitHandler}>
+        <input type="search" value={searchInput} onChange={updateSearchInput} />
+        <select value={type} onChange={updateType}></select>
+        {type && <select value={field} onChange={updateField}></select>}
+      </form>
     </div>
   );
 };
