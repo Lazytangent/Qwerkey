ca94ad14bdeca725951f6c0e7f39b4f65013f89d
Lazytangent <pm123633@gmail.com>
2021-03-11 16:35:53 -0600

    Created components for R of communities

---
+++ react-app/src/components/CommunitiesContainer/CommunitiesContainer.js
@@ -0,0 +1,9 @@
+const CommunitiesContainer = () => {
+  return (
+    <>
+      <h3>Placeholder for CommunitiesContainer</h3>
+    </>
+  );
+};
+
+export default CommunitiesContainer;
---
+++ react-app/src/components/Community/Community.js
@@ -0,0 +1,9 @@
+const Community = () => {
+  return (
+    <>
+      <h3>Placeholder for Community</h3>
+    </>
+  );
+};
+
+export default Community;
