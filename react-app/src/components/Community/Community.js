e5e8c47ecfa56e57cfd35d9f6fbb702fc02312ce
Lazytangent <pm123633@gmail.com>
2021-03-11 16:39:18 -0600

    Start detailing out Community card

--- react-app/src/components/Community/Community.js
+++ react-app/src/components/Community/Community.js
@@ -1,4 +1,26 @@
-const Community = () => {
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import EditButton from "../parts/EditButton";
+import DeleteButton from "../parts/DeleteButton";
+import EditCommunityModal from "../EditCommunityForm";
+import DeleteConfirmationModal from "../parts/DeleteConfirmation";
+
+const Community = ({ community }) => {
+  const user = useSelector(state => state.session.user);
+
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const editBtnHandler = () => {
+    setShowEditModal(true);
+  };
+
+  const deleteBtnHandler = () => {
+    setShowDeleteModal(true);
+  };
+
   return (
     <>
       <h3>Placeholder for Community</h3>
