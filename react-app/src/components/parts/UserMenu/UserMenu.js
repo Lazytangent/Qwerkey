81a71ccf0f11cf0b8bb4738c1cc96ab24aef7302
Lazytangent <pm123633@gmail.com>
2021-03-11 13:14:30 -0600

    Working on transition for UserMenu

--- react-app/src/components/parts/UserMenu/UserMenu.js
+++ react-app/src/components/parts/UserMenu/UserMenu.js
@@ -5,7 +5,7 @@ import NavButton from "../NavButton";
 
 const UserMenu = ({ createPostBtnHandler }) => {
   return (
-    <div className="absolute right-0 flex flex-col items-center p-2 rounded z-10 top-16 dark:bg-gray-600">
+    <div className="absolute right-0 z-10 flex flex-col items-center p-2 bg-gray-200 rounded top-16 dark:bg-gray-600">
       <NavButton name="Create Post" onClick={createPostBtnHandler}>
         <CreatePostModal />
       </NavButton>
