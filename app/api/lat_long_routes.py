ca5b53ad39552b834d17ecc20988ac44a52b755f
Lazytangent <pm123633@gmail.com>
2021-03-12 12:26:33 -0600

    Setting up config vars

--- app/api/lat_long_routes.py
+++ app/api/lat_long_routes.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, request
 
 lat_long_routes = Blueprint("lat_long", __name__)
+
+
+@lat_long_routes.route('/')
+    pass
--- app/config.py
+++ app/config.py
@@ -11,3 +11,5 @@ class Config:
     S3_KEY = os.environ.get("S3_ACCESS_KEY")
     S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
     S3_LOCATION = f"http://{S3_BUCKET}.s3.amazonaws.com/"
+
+    OPEN_CAGE_API_KEY = os.environ.get("OPEN_CAGE_API_KEY")
