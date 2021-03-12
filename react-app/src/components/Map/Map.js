219e181a91028b4e9779cf49a431c3c3030641d6
Lazytangent <pm123633@gmail.com>
2021-03-12 15:39:13 -0600

    Fix styling issue with Popup

--- react-app/src/components/Map/Map.js
+++ react-app/src/components/Map/Map.js
@@ -1,19 +1,18 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Map = ({ long, lat }) => {
-
   return (
     <div className="map__container">
-      <MapContainer
-        center={[long, lat]}
-        zoom={13}
-        style={{ height: "400px" }}
-      >
+      <MapContainer center={[long, lat]} zoom={13} style={{ height: "400px" }}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker position={[long, lat]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
