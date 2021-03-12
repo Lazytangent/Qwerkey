b86cbd52b5c5cf82188e43d023d58c9ee75a0450
Lazytangent <pm123633@gmail.com>
2021-03-12 13:04:48 -0600

    Start routing for creating lat and longs

--- app/api/lat_long_routes.py
+++ app/api/lat_long_routes.py
@@ -1,7 +1,18 @@
+import requests
 from flask import Blueprint, request
+
+from app.config import Config
 
 lat_long_routes = Blueprint("lat_long", __name__)
 
 
 @lat_long_routes.route('/')
-    pass
+def get_lat_long():
+    city = request.args.get("city")
+    state = request.args.get("state")
+    response = \
+        requests.get(
+            f"""https://api.opencagedata.com/geocode/v1/json?key={Config.OPEN_CAGE_API_KEY}
+\           &q={city},{state},USA""")
+    print(response)
+    return response.json()
