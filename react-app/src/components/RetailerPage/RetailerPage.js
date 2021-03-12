caf989cf2d237dc5a6a20d25c9332aacd45a3200
Lazytangent <pm123633@gmail.com>
2021-03-12 13:36:13 -0600

    Added demo code for leaflet

--- react-app/src/components/RetailerPage/RetailerPage.js
+++ react-app/src/components/RetailerPage/RetailerPage.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import { getOneRetailer } from "../../store/retailers";
 import Retailer from "../Retailer";
@@ -10,8 +11,10 @@ import RetailerRatingForm from "../RetailerRatingForm";
 const RetailerPage = () => {
   const { retailerId } = useParams();
   const dispatch = useDispatch();
-  const retailer = useSelector(state => state.retailers.retailers[retailerId]);
-  const user = useSelector(state => state.session.user);
+  const retailer = useSelector(
+    (state) => state.retailers.retailers[retailerId]
+  );
+  const user = useSelector((state) => state.session.user);
 
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -34,8 +37,25 @@ const RetailerPage = () => {
       {retailer && (
         <>
           <Retailer retailer={retailer} />
-          {user && retailer.owner.id !== user.id && (<RetailerRatingForm retailerId={retailer.id} />)}
+          {user && retailer.owner.id !== user.id && (
+            <RetailerRatingForm retailerId={retailer.id} />
+          )}
           <RetailerRatingsContainer retailerId={retailer.id} />
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </>
       )}
     </>
