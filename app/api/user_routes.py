2b274ffcfaef06db9d6ad5f94f49cd7099c0e5c4
Lazytangent <pm123633@gmail.com>
2021-03-13 22:29:38 -0600

    Add max route for users

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -17,3 +17,9 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/max')
+def get_max_number_of_users():
+    number = User.query.count()
+    return {"max": number}
