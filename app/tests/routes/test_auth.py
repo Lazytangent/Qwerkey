46b66c204b03e202caeb7813654ee2f7c2b6918b
Lazytangent <pm123633@gmail.com>
2021-03-13 20:39:41 -0600

    Working first pytest

--- app/tests/routes/test_auth.py
+++ app/tests/routes/test_auth.py
@@ -13,10 +13,16 @@ def test_valid_login(client):
         password="password"
     )
 
+    response = client.get('/api/auth')
+    csrf_token = response.headers['Set-Cookie']
+
     response = client.post('/api/auth/login', json={
         'credential': "johnthetester",
         'password': "password"
-    })
+    }, headers={'csrf_token': csrf_token})
     json_response = response.get_json()
-    print('-----------------', json_response)
-    assert False
+    assert response.status_code == 200
+
+    user = User.query.filter_by(email='john@test.com').first()
+    assert user is not None
+    assert user.username == "johnthetester"
