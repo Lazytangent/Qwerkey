28c307d8dcc68bccd79aa1dc2efb458591e549b3
Lazytangent <pm123633@gmail.com>
2021-03-13 22:28:29 -0600

    Start thunks

--- react-app/src/store/users.js
+++ react-app/src/store/users.js
@@ -1,3 +1,23 @@
+const SET_MAX = "users/SET_MAX";
+
+const setMaxNumberOfUsers = (number) => {
+  return {
+    type: SET_MAX,
+    number,
+  };
+};
+
+export const getUsers = (page) => async (dispatch) => {
+
+};
+
+export const getMaxNumberOfUsers = () => async (dispatch) => {
+  const res = await fetch('/api/users/max');
+  const number = await res.json();
+  dispatch(setMaxNumberOfUsers(number.max));
+  return number;
+};
+
 const initialState = {
   users: {},
   max: null,
@@ -5,6 +25,8 @@ const initialState = {
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_MAX:
+      return { ...state, max: action.number };
     default:
       return state;
   }
