275f7b5ec0fd9c0868ae6f83a3ec2771c1585632
Lazytangent <pm123633@gmail.com>
2021-03-14 14:58:25 -0500

    Setting up component

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { getUser } from "../../store/users";
+
 const ProfilePage = () => {
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.users.users[userId]);
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(getUser(userId));
+  }, [dispatch, userId]);
+
+  useEffect(() => {
+    if (user) {
+      setIsLoaded(true);
+    }
+  }, [user]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <>
       <h3>Placeholder for ProfilePage</h3>
