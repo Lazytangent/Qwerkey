58729e87b703d831fbf116083e9f64e4cad95f55
Lazytangent <pm123633@gmail.com>
2021-03-15 15:16:42 -0500

    Update sidebar content

--- react-app/src/components/Sidebar/Sidebar.js
+++ react-app/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 import { getPopularCommunities } from "../../store/sidebar";
 import About from "../About";
 
 const Sidebar = () => {
+  const location = useLocation();
   const dispatch = useDispatch();
   const popularCommunities = useSelector((state) => state.sidebar.popular);
   const currentCommunity = useSelector((state) => state.sidebar.community);
@@ -27,7 +28,7 @@ const Sidebar = () => {
 
   return (
     <div className="fixed max-w-xs p-2 pt-0">
-      {currentCommunity && (
+      {location.pathname.startsWith("/q") && currentCommunity && (
         <div className="max-w-md p-2 border border-gray-600 rounded">
           <h3 className="text-center">{currentCommunity.name}</h3>
           <p className="p-2">{currentCommunity.description}</p>
