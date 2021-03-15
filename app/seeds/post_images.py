fffdbe4b243ded3e5b6d79e72cfd3f60a57c861e
Lazytangent <pm123633@gmail.com>
2021-03-14 21:59:27 -0500

    Create seed functions

---
+++ app/seeds/post_images.py
@@ -0,0 +1,18 @@
+import json
+from app.models import db, PostsImage
+
+
+def seed_post_images():
+    new_images = []
+    with open('./app/seeds/post_images.json') as f:
+        data = json.load(f)
+        for image in data:
+            new_image = PostsImage(**image)
+            new_images.append(new_image)
+
+    db.session.add_all(new_images)
+    db.session.commit()
+
+def undo_post_images():
+    db.session.execute('TRUNCATE post_images RESTART IDENTITY CASCADE;')
+    db.session.commit()
