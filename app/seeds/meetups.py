d02499d44f9065ba9dcbc8a7a4db44e84fb11f57
Lazytangent <pm123633@gmail.com>
2021-03-22 21:07:02 -0500

    Create meetup seed data

---
+++ app/seeds/meetups.py
@@ -0,0 +1,18 @@
+import json
+from app.models import db, Meetup
+
+
+def seed_meetups():
+    new_meetups = []
+    with open('./app/seeds/meetups.json') as f:
+        data = json.load(f)
+        for meetup in data:
+            new_meetup = Meetup(**meetup)
+            new_meetups.append(new_meetup)
+
+    db.session.add_all(new_meetups)
+    db.session.commit()
+
+def undo_meetups():
+    db.session.execute('TRUNCATE meetups RESTART IDENTITY CASCADE;')
+    db.session.commit()
