5c22d2e28786e87c08a9fe9d42ebb55a1796ec7c
Lazytangent <pm123633@gmail.com>
2021-03-22 19:03:30 -0500

    Draft API routes

--- app/api/meetup_routes.py
+++ app/api/meetup_routes.py
@@ -1,3 +1,25 @@
 from flask import Blueprint, request
 
+from app.models import db, Meetup
+
 meetup_routes = Blueprint("meetups", __name__)
+
+
+@meetup_routes.route('')
+def get_meetups():
+    pass
+
+
+@meetup_routes.route('', methods=["POST"])
+def create_meetup():
+    pass
+
+
+@meetup_routes.route('/<int:meetup_id>')
+def get_meetup_by_id(meetup_id):
+    pass
+
+
+@meetup_routes.route('/<int:meetup_id>', methods=["PUT", "DELETE"])
+def update_meetup(meetup_id):
+    pass
