a83931ee409143da029610af1f12ffbbe3abe244
Lazytangent <pm123633@gmail.com>
2021-03-25 17:53:27 -0500

    Set up MeetupPage

--- react-app/src/components/MeetupPage/MeetupPage.js
+++ react-app/src/components/MeetupPage/MeetupPage.js
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { getMeetupById } from "../../store/meetups";
+import Meetup from "../Meetup";
+import Map from "../Map";
+
 const MeetupPage = () => {
+  const { meetupId } = useParams();
+  const dispatch = useDispatch();
+  const meetup = useSelector((state) => state.meetups.meetups[meetupId]);
+  const user = useSelector((state) => state.session.user);
+
+  useEffect(() => {
+    dispatch(getMeetupById(meetupId));
+  }, [dispatch, meetupId]);
+
   return (
     <>
-      <h3>Placeholder for MeetupPage</h3>
+      <Meetup meetup={meetup} />
     </>
   );
 };
