6cb72b7ac890290eed8ec93bdaca56d4488e4db0
Lazytangent <pm123633@gmail.com>
2021-03-25 18:03:22 -0500

    Set up MeetupPage with Map

--- react-app/src/components/MeetupPage/MeetupPage.js
+++ react-app/src/components/MeetupPage/MeetupPage.js
@@ -1,24 +1,45 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
-import { getMeetupById } from "../../store/meetups";
+import { getMeetupById, getMeetupLocation } from "../../store/meetups";
 import Meetup from "../Meetup";
 import Map from "../Map";
 
 const MeetupPage = () => {
   const { meetupId } = useParams();
+
   const dispatch = useDispatch();
   const meetup = useSelector((state) => state.meetups.meetups[meetupId]);
   const user = useSelector((state) => state.session.user);
+
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     dispatch(getMeetupById(meetupId));
   }, [dispatch, meetupId]);
 
+  useEffect(() => {
+    (async () => {
+      if (meetup) {
+        if (!(meetup.lng || meetup.lat)) {
+          await dispatch(getMeetupLocation(meetupId));
+        }
+        setIsLoaded(true);
+      }
+    })();
+  }, [dispatch, meetup, meetupId]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <>
       <Meetup meetup={meetup} />
+      {meetup.lng && meetup.lat && (
+        <Map long={meetup.lng} lat={meetup.lat} />
+      )}
     </>
   );
 };
