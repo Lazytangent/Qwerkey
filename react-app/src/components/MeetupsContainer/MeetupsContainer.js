9fc47670e2a02ebdd1ab8be927dfc8e173f28f0f
Lazytangent <pm123633@gmail.com>
2021-03-25 17:17:45 -0500

    Configure base useEffect

--- react-app/src/components/MeetupsContainer/MeetupsContainer.js
+++ react-app/src/components/MeetupsContainer/MeetupsContainer.js
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getMeetups } from "../../store/meetups";
+import Meetup from "../Meetup";
+
 const MeetupsContainer = () => {
+  const dispatch = useDispatch();
+  const meetups = useSelector((state) => state.meetups.meetups);
+
+  useEffect(() => {
+    dispatch(getMeetups(1));
+  }, [dispatch]);
+
   return (
     <>
-      <h3>Placeholder for MeetupsContainer</h3>
+      {Object.values(meetups).map(meetup => (
+        <Meetup meetup={meetup} />
+      ))}
     </>
   );
 };
