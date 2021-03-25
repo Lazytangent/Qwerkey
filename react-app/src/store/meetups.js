f9b20ef254aa430a5f95e3e44e8d9fd9eb3721dc
Lazytangent <pm123633@gmail.com>
2021-03-24 19:03:17 -0500

    Add meetups slice of state

---
+++ react-app/src/store/meetups.js
@@ -0,0 +1,12 @@
+const initialState = {
+  meetups: {},
+};
+
+const meetupsReducer = (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default meetupsReducer;
