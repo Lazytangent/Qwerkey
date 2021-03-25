70d3be76f0c6da64b4a827bd26497a812d082db9
Lazytangent <pm123633@gmail.com>
2021-03-24 19:17:21 -0500

    Draft action creators

--- react-app/src/store/meetups.js
+++ react-app/src/store/meetups.js
@@ -1,3 +1,20 @@
+const SET_MEETUPS = "meetups/SET_MEETUPS";
+const SET_MEETUP = "meetups/SET_MEETUP";
+
+const setMeetups = (meetups) => {
+  return {
+    type: SET_MEETUPS,
+    meetups,
+  };
+};
+
+const setMeetup = (meetup) => {
+  return {
+    type: SET_MEETUP,
+    meetup,
+  };
+};
+
 const initialState = {
   meetups: {},
 };
