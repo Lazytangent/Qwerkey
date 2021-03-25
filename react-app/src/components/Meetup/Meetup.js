407064434e8b98d1038bd220c36946055ae173ba
Lazytangent <pm123633@gmail.com>
2021-03-25 17:34:54 -0500

    Meetup

--- react-app/src/components/Meetup/Meetup.js
+++ react-app/src/components/Meetup/Meetup.js
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 
 import DivCard from "../parts/DivCard";
+import UserName from "../parts/UserName";
+import options from "../../utils/localeDateString";
 
 const Meetup = ({ meetup }) => {
   return (
@@ -12,6 +14,14 @@ const Meetup = ({ meetup }) => {
           </span>
         </NavLink>
       </h3>
+      <p className="p-2">{meetup.description}</p>
+      <hr />
+      <p className="p-2">
+        Organized by <UserName link={`/users/${meetup.user.id}`} username={meetup.user.username} />
+      </p>
+      <p className="p-2">
+        Scheduled for {new Date(meetup.date).toLocaleString(...options())} in {meetup.city}, {meetup.state}
+      </p>
     </DivCard>
   );
 };
