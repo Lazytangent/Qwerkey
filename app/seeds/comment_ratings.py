6e19c8bdb5dc270e8a8e7638556059d1e0c2d15f
Lazytangent <pm123633@gmail.com>
2021-03-25 23:11:11 -0500

    Set up seed files

---
+++ app/seeds/comment_ratings.py
@@ -0,0 +1,18 @@
+import json
+from app.models import db, CommentRating
+
+
+def seed_comment_ratings():
+    new_ratings = []
+    with open('./app/seeds/comment_ratings.json') as f:
+        data = json.load(f)
+        for rating in data:
+            new_rating = CommentRating(**rating)
+            new_ratings.append(new_rating)
+
+    db.session.add_all(new_ratings)
+    db.session.commit()
+
+def undo_comment_ratings():
+    db.session.execute('TRUNCATE comment_ratings RESTART IDENTITY CASCADE;')
+    db.session.commit()
---
+++ app/seeds/post_ratings.py
@@ -0,0 +1,18 @@
+import json
+from app.models import db, PostRating
+
+
+def seed_post_ratings():
+    new_ratings = []
+    with open('./app/seeds/post_ratings.json') as f:
+        data = json.load(f)
+        for rating in data:
+            new_rating = PostRating(**rating)
+            new_ratings.append(new_rating)
+
+    db.session.add_all(new_ratings)
+    db.session.commit()
+
+def undo_post_ratings():
+    db.session.execute('TRUNCATE post_ratings RESTART IDENTITY CASCADE;')
+    db.session.commit()
