9fc24dfb34cafffd05056da435de45a87f4ccde3
Lazytangent <pm123633@gmail.com>
2021-03-25 19:42:52 -0500

    Begin infinite scroll to meetups container

--- react-app/src/components/MeetupsContainer/MeetupsContainer.js
+++ react-app/src/components/MeetupsContainer/MeetupsContainer.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
-import { getMeetups } from "../../store/meetups";
+import { getMeetups, getMaxNumberOfMeetups } from "../../store/meetups";
 import Meetup from "../Meetup";
 
 const MeetupsContainer = () => {
   const dispatch = useDispatch();
   const meetups = useSelector((state) => state.meetups.meetups);
+  const maxMeetups = useSelector((state) => state.meeups.max);
+
+  const [page, setPage] = useState(1);
+  const [currentMeetups, setCurrentMeetups] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(getMaxNumberOfMeetups());
+  }, [dispatch]);
 
   useEffect(() => {
     dispatch(getMeetups(1));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (page * 20 - maxMeetups < 20) {
+      dispatch(getMeetups(page));
+    }
+  }, [dispatch, page, maxMeetups]);
 
   return (
     <>
