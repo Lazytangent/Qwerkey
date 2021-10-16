f889547d8f14e32c04072930c984b95b5fd9ebaf
Lazytangent <pm123633@gmail.com>
2021-10-16 18:43:49 -0500

    Clean up `src/index.js` entry file
    * Create `ContextProvider` to encapsulate Context Providers
    * Remove Redux action imports from store files

---
+++ react-app/src/context/index.js
@@ -0,0 +1,30 @@
+import ModalProvider from "./context/ModalContext";
+import AuthProvider from "./context/AuthContext";
+import CreatePostProvider from "./context/CreatePostContext";
+import CommentProvider from "./context/CommentContext";
+import DarkModeProvider from "./context/DarkModeContext";
+import RetailerRatingProvider from "./context/RetailerRatingContext";
+import CollapsedSidebarProvider from "./context/CollapsedSidebarContext";
+import SearchProvider from "./context/SearchContext";
+
+const ContextProvider = ({ children }) => (
+  <ModalProvider>
+    <AuthProvider>
+      <CreatePostProvider>
+        <CommentProvider>
+          <DarkModeProvider>
+            <RetailerRatingProvider>
+              <CollapsedSidebarProvider>
+                <SearchProvider>
+                  {children}
+                </SearchProvider>
+              </CollapsedSidebarProvider>
+            </RetailerRatingProvider>
+          </DarkModeProvider>
+        </CommentProvider>
+      </CreatePostProvider>
+    </AuthProvider>
+  </ModalProvider>
+);
+
+export default ContextProvider;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,58 +6,20 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import configureStore from "./store";
-import ModalProvider from "./context/ModalContext";
-import AuthProvider from "./context/AuthContext";
-import CreatePostProvider from "./context/CreatePostContext";
-import CommentProvider from "./context/CommentContext";
-import DarkModeProvider from "./context/DarkModeContext";
-import RetailerRatingProvider from "./context/RetailerRatingContext";
-import CollapsedSidebarProvider from "./context/CollapsedSidebarContext";
-import SearchProvider from "./context/SearchContext";
-
-import * as sessionActions from "./store/session";
-import * as userActions from "./store/users";
-import * as postActions from "./store/posts";
-import * as retailerActions from "./store/retailers";
-import * as searchActions from "./store/search";
-import * as communityActions from "./store/communities";
-import * as sidebarActions from "./store/sidebar";
-import * as meetupActions from "./store/meetups";
+import ContextProvider from './context';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
-  window.sessionActions = sessionActions;
-  window.userActions = userActions;
-  window.postActions = postActions;
-  window.retailerActions = retailerActions;
-  window.searchActions = searchActions;
-  window.communityActions = communityActions;
-  window.sidebarActions = sidebarActions;
-  window.meetupActions = meetupActions;
 }
 
 const Root = () => (
   <BrowserRouter>
     <Provider store={store}>
-      <ModalProvider>
-        <AuthProvider>
-          <CreatePostProvider>
-            <CommentProvider>
-              <DarkModeProvider>
-                <RetailerRatingProvider>
-                  <CollapsedSidebarProvider>
-                    <SearchProvider>
-                      <App />
-                    </SearchProvider>
-                  </CollapsedSidebarProvider>
-                </RetailerRatingProvider>
-              </DarkModeProvider>
-            </CommentProvider>
-          </CreatePostProvider>
-        </AuthProvider>
-      </ModalProvider>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </Provider>
   </BrowserRouter>
 );
