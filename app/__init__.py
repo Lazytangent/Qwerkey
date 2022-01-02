b81b2cad31d8288fb7dbbf7e6446218e8adfedcb
Lazytangent <pm123633@gmail.com>
2022-01-01 20:20:28 -0600

    Remove unused imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, request, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_wtf.csrf import generate_csrf
 from flask_login import LoginManager
 
 from .api import (
