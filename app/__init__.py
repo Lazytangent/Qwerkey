f7f46eb1bb33334f85d6bd1f3c81a3c32a22ee79
Lazytangent <pm123633@gmail.com>
2022-01-09 21:18:15 -0600

    Refactor blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -6,18 +6,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import generate_csrf
 
-from .api import (
-    auth_routes,
-    comment_routes,
-    community_routes,
-    lat_long_routes,
-    meetup_routes,
-    post_routes,
-    posts_image_routes,
-    retailer_routes,
-    search_routes,
-    user_routes,
-)
+from .api import api
 from .config import Config
 from .models import User, db
 from .seeds import seed_commands
@@ -28,7 +17,7 @@ def create_app(testing=False):
 
     # Setup login manager
     login = LoginManager(app)
-    login.login_view = "auth.unauthorized"
+    login.login_view = "api.auth.unauthorized"
 
     @login.user_loader
     def load_user(id):
@@ -37,16 +26,7 @@ def create_app(testing=False):
     app.cli.add_command(seed_commands)
 
     app.config.from_object(Config)
-    app.register_blueprint(user_routes, url_prefix="/api/users")
-    app.register_blueprint(auth_routes, url_prefix="/api/auth")
-    app.register_blueprint(post_routes, url_prefix="/api/posts")
-    app.register_blueprint(posts_image_routes, url_prefix="/api/post_images")
-    app.register_blueprint(comment_routes, url_prefix="/api/comments")
-    app.register_blueprint(retailer_routes, url_prefix="/api/retailers")
-    app.register_blueprint(search_routes, url_prefix="/api/search")
-    app.register_blueprint(community_routes, url_prefix="/api/communities")
-    app.register_blueprint(lat_long_routes, url_prefix="/api/lat_long")
-    app.register_blueprint(meetup_routes, url_prefix="/api/meetups")
+    app.register_blueprint(api, url_prefix="/api")
     db.init_app(app)
     Migrate(app, db)
 
@@ -82,3 +62,5 @@ def create_app(testing=False):
         if path == "favicon.ico":
             return app.send_static_file("favicon.ico")
         return app.send_static_file("index.html")
+
+    return app
