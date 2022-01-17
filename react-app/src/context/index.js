51d98f7e094709fcb901fdbe70953108c277ab63
Lazytangent <peter.tn.mai@gmail.com>
2022-01-17 17:24:05 -0600

    Run pre-commit

--- react-app/src/context/index.js
+++ react-app/src/context/index.js
@@ -1,11 +1,11 @@
-import ModalProvider from "./context/ModalContext";
-import AuthProvider from "./context/AuthContext";
-import CreatePostProvider from "./context/CreatePostContext";
-import CommentProvider from "./context/CommentContext";
-import DarkModeProvider from "./context/DarkModeContext";
-import RetailerRatingProvider from "./context/RetailerRatingContext";
-import CollapsedSidebarProvider from "./context/CollapsedSidebarContext";
-import SearchProvider from "./context/SearchContext";
+import ModalProvider from './context/ModalContext';
+import AuthProvider from './context/AuthContext';
+import CreatePostProvider from './context/CreatePostContext';
+import CommentProvider from './context/CommentContext';
+import DarkModeProvider from './context/DarkModeContext';
+import RetailerRatingProvider from './context/RetailerRatingContext';
+import CollapsedSidebarProvider from './context/CollapsedSidebarContext';
+import SearchProvider from './context/SearchContext';
 
 const ContextProvider = ({ children }) => (
   <ModalProvider>
@@ -15,9 +15,7 @@ const ContextProvider = ({ children }) => (
           <DarkModeProvider>
             <RetailerRatingProvider>
               <CollapsedSidebarProvider>
-                <SearchProvider>
-                  {children}
-                </SearchProvider>
+                <SearchProvider>{children}</SearchProvider>
               </CollapsedSidebarProvider>
             </RetailerRatingProvider>
           </DarkModeProvider>
