7b7b632eb96e9e23174b56ccd2cd3f267ab73457
Lazytangent <peter.tn.mai@gmail.com>
2022-03-12 21:14:04 -0600

    Remove manual injection of csrf_token

--- app/routes/api/auth.py
+++ app/routes/api/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from flask_login import current_user, login_user, logout_user
 from sqlalchemy import or_
 
@@ -27,7 +27,6 @@ def login():
     form = LoginForm()
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
-    form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
         credential = form.data["credential"]
         user = User.query.filter(
@@ -53,7 +52,6 @@ def sign_up():
     Creates a new user and logs them in
     """
     form = SignUpForm()
-    form["csrf_token"].data = request.cookies["csrf_token"]
     if form.validate_on_submit():
         user = User(
             username=form.data["username"],
