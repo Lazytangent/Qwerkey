68976f13eb294672d1f9d09e2eacc03f4fe96629
Lazytangent <peter.tn.mai@gmail.com>
2022-03-12 20:29:04 -0600

    Inject csrf cookie into header of fetch api

--- react-app/src/store/api.js
+++ react-app/src/store/api.js
@@ -1,8 +1,13 @@
+import Cookies from 'js-cookie';
+
 export default function api(url, method = 'GET', body, isFormData = false) {
   if (!isFormData) {
     return fetch(url, {
       method,
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': Cookies.get('csrf_token'),
+      },
       body: JSON.stringify(body),
     });
   } else {
