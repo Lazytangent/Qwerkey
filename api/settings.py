5ae5d36435de072a44318fc312331cd334061092
Lazytangent <peter.tn.mai@gmail.com>
2023-07-03 13:58:11 -0500

    Migrate settings

--- api/settings.py
+++ api/settings.py
@@ -1,18 +1,46 @@
 from enum import Enum
 
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EnvEnum(str, Enum):
-    prod = 'PROD'
-    staging = 'STAGING'
-    dev = 'DEV'
+    prod = "PROD"
+    staging = "STAGING"
+    dev = "DEV"
+
+
+class SqlalchemySettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="sqlalchemy_")
+
+    track_modifications: bool = False
+    echo: bool = False
+
+
+class AwsS3Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="s3_")
+
+    bucket: str
+    access_key: str
+    secret_access_key: str
+
+    @computed_field
+    @property
+    def location(self) -> str:
+        return f"http://{self.bucket}.s3.amazonaws.com/"
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     environment: EnvEnum = EnvEnum.dev
+    secret_key: str
+    database_url: PostgresDsn
+
+    open_cage_api_key: str
+
+    sqlalchemy_settings: SqlalchemySettings = SqlalchemySettings()
+    aws_s3_settings: AwsS3Settings = AwsS3Settings()
 
     @property
     def is_dev(self) -> bool:
