c80fd1e5faebae9fa18310392112a1fda45d2104
Lazytangent <peter.tn.mai@gmail.com>
2023-07-03 19:39:42 -0500

    Create basic healthcheck endpoint

---
+++ api/constants.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Tags(str, Enum):
+    api = "API"
+    healthcheck = "HEALTHCHECK"
--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.routers import api
 from api.settings import settings
 
 app = FastAPI()
@@ -17,6 +18,8 @@ if settings.is_dev:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+app.include_router(api.router)
 
 
 @app.get("/")
