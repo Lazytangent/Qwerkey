9add14b82243b3d4854b2a23c7aceaecb46421ee
Lazytangent <peter.tn.mai@gmail.com>
2023-07-05 07:09:17 -0500

    Use react-dom v18 API

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -22,9 +22,10 @@ const Root = () => (
   </BrowserRouter>
 );
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
